62afa6c1298379013e5a79842cd81f318589c443
Geoff (mandrake) Harrison <mandrake@mandrake.net>
2013-08-25 16:42:35 -0400

    Add network stub, game play screen

---
+++ Rat Slap Touch/RSNetworkController.h
@@ -0,0 +1,13 @@
+//
+//  RSNetworkContorller.h
+//  Rat Slap Touch
+//
+//  Created by mandrake on 8/25/13.
+//  Copyright (c) 2013 mandrake. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSNetworkController : NSObject
+
+@end
---
+++ Rat Slap Touch/RSNetworkController.m
@@ -0,0 +1,13 @@
+//
+//  RSNetworkContorller.m
+//  Rat Slap Touch
+//
+//  Created by mandrake on 8/25/13.
+//  Copyright (c) 2013 mandrake. All rights reserved.
+//
+
+#import "RSNetworkController.h"
+
+@implementation RSNetworkController
+
+@end
