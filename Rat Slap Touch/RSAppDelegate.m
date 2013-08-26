f23ca255d452d1b27c459a96f62558efd967d762
Geoff (mandrake) Harrison <mandrake@mandrake.net>
2013-08-25 21:36:54 -0400

    Move assert cleanup to the main.m file

--- Rat Slap Touch/RSAppDelegate.m
+++ Rat Slap Touch/RSAppDelegate.m
@@ -11,24 +11,9 @@
 
 @implementation RSAppDelegate
 
-typedef int (*PYStdWriter)(void *, const char *, int);
 
-static PYStdWriter _oldStdWrite;
-
-int __pyStderrWrite(void *inFD, const char *buffer, int size) {
-    if ( strncmp(buffer, "AssertMacros:", 13) == 0 ) {
-        return 0;
-    }
-    return _oldStdWrite(inFD, buffer, size);
-}
-
-void __iOS7B5CleanConsoleOutput(void) {
-    _oldStdWrite = stderr->_write;
-    stderr->_write = __pyStderrWrite;
-}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    __iOS7B5CleanConsoleOutput();
     NSLog(@"Delegate: Initializing");
     gameType = NO_CURRENT_GAME;
     return YES;
